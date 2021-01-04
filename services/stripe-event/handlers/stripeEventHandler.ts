91a8155ca4d97c436a7b453e48374d1904662812
Arjun Iyer <arjun@getwisely.com>
2021-01-03 17:38:11 -0800

    cleaned up stripeEventHandler

---
+++ services/stripe-event/handlers/stripeEventHandler.ts
@@ -0,0 +1,47 @@
+import Stripe from 'stripe'
+import * as AWS  from 'aws-sdk'
+      
+
+const stripe = new Stripe(process.env.STRIPE_API_KEY, {
+  apiVersion: '2020-08-27',
+  typescript: true,
+});
+const secretsManager: AWS.SecretsManager = new AWS.SecretsManager({
+  endpoint: `http://${process.env.LOCALSTACK_HOSTNAME}:4566`,
+  region: process.env.AWS_DEFAULT_REGION
+})
+const eventBridge: AWS.EventBridge = new AWS.EventBridge({
+  endpoint: `http://${process.env.LOCALSTACK_HOSTNAME}:4566`,
+  region: process.env.AWS_DEFAULT_REGION
+});
+
+const handler = async (event) => {
+  let err = null
+  try {
+    console.log('*************** START ********************')
+    const signature: string = event.headers["Stripe-Signature"]
+    const secret: string = (await (secretsManager.getSecretValue({ SecretId: process.env.STRIPE_SIGNING_SECRET })).promise()).SecretString
+    
+    const eventReceived: Stripe.Event = stripe.webhooks.constructEvent(event.body, signature, secret)
+    
+    await eventBridge.putEvents({
+      Entries: [
+        {
+          Source: 'stripe',
+          DetailType: eventReceived.type,
+          Detail: JSON.stringify(eventReceived),
+        },
+      ]
+    }).promise();
+  } catch (e) {
+    err = e
+    console.log('*************** ERROR ********************')
+    console.log(e)
+    console.log('******************************************')
+  }
+  const body = err ? JSON.stringify(err) : ""
+  const statusCode = err ? 500 : 200
+  return { statusCode, body }
+};
+
+export { handler }
