5afe70e33375718cbab2d42a3190afe0d0628699
Arjun <arjuna.r.iyer@gmail.com>
2021-01-14 20:54:10 -0800

    Update Dashboard.tsx

--- ui/react-dashboard/src/pages/Dashboard.tsx
+++ ui/react-dashboard/src/pages/Dashboard.tsx
@@ -85,18 +85,19 @@ function Dashboard() {
      <div className={classes.appBarSpacer} />
         <Container maxWidth="lg" className={classes.container}>
           <Grid container spacing={3}>
+            {/* Categories */}
             <Grid item xs={12} md={8} lg={9}>
               <Paper className={fixedHeightPaper}>
               <TransactionCategoryChart></TransactionCategoryChart>
               </Paper>
             </Grid>
-            {/* Recent Deposits */}
+            {/* Metrics */}
             <Grid item xs={12} md={4} lg={3}>
               <Paper className={fixedHeightPaper}>
                 <TransactionMetrics></TransactionMetrics>
               </Paper>
             </Grid>
-            {/* Recent Transactions */}
+            {/* Transactions */}
             <Grid item xs={12}>
               <Paper className={classes.paper}>
                 <TransactionList></TransactionList>
